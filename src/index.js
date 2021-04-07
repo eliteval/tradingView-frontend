74ba6d2a28e1b4a85df7c04f069031c8a96d8d69
vladimir9255 <70789419+darwin12112@users.noreply.github.com>
2021-04-07 23:20:31 +0900

    insert to wordpress

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 ReactDOM.render(
 	<Router>
 		<Switch>
-			<Route path="/:balance/:currency/:ticker/:time_frame/:start_date/:end_date/:user_id/:session_strategy_id" component={App} />
+			<Route path="/:balance?/:currency?/:ticker?/:time_frame?/:start_date?/:end_date?/:user_id?/:session_strategy_id?" component={App} />
 		</Switch>
 	</Router>,
 	document.getElementById('root')
