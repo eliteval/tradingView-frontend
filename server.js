60970d470c4981168bec05ee4673997cd397f311
vladimir9255 <70789419+darwin12112@users.noreply.github.com>
2021-04-29 15:45:47 +0900

    fix

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+
+const port = 5001;
+
+app.use(cors());
+// create our router
+const router = express.Router();
+app.use('/static',express.static(path.resolve(__dirname, "build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
+
+module.exports = app;
